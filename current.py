1b1fe45da7706eca18836bd1ae29323c0364c22c
HDbug <db.dan19.db@gmail.com>
2019-03-20 21:05:59 +0000

    Created check for existing folders

--- current.py
+++ current.py
@@ -4,15 +4,22 @@ home = os.environ['USERPROFILE'] + '\\Downloads'
 files = os.listdir(home)
 sfile = ''
 
-def check(uinput):
-    fnmatch.fnmatch()
+def checkR(uinput):
+    fnmatch.fnmatch(sfile, uinput)
 
 for sfile in files:
     filename, fileExtension = os.path.splitext('%s\\%s' % (home, sfile))
     check = os.path.isdir('%s\\%s' % (home, sfile))
+    exists = os.path.exists(home + '\\' + fileExtension)
+    fileExtensionRemoved = fileExtension.replace('.', '')
     #fnmatch.fnmatch(sfile, )
 
     if check == True:
       print('%s is a folder' %(sfile))
     elif check == False:
       print('file (%s) with the extension of (%s)' % (sfile, fileExtension))
+      if exists == True:
+        print('Folder %s already exists, not creating' %(fileExtension))
+      elif exists == False:
+        os.mkdir(home + '\\' + fileExtensionRemoved)
+        
