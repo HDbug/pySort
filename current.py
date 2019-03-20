100a4d1d38227902c56f3f628a7ea8f16f63c7f7
HDbug <db.dan19.db@gmail.com>
2019-03-20 20:28:56 +0000

    Added correct checking and listing file extensions

--- current.py
+++ current.py
@@ -2,9 +2,17 @@ import os, shutil, fnmatch
 
 home = os.environ['USERPROFILE'] + '\\Downloads'
 files = os.listdir(home)
+sfile = ''
 
 def check(uinput):
     fnmatch.fnmatch()
 
 for sfile in files:
-    
+    filename, fileExtension = os.path.splitext('%s\\%s' % (home, sfile))
+    check = os.path.isdir('%s\\%s' % (home, sfile))
+    #fnmatch.fnmatch(sfile, )
+
+    if check == True:
+      print('%s is a folder' %(sfile))
+    elif check == False:
+      print('file (%s) with the extension of (%s)' % (sfile, fileExtension))
